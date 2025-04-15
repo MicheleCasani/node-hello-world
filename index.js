413ad79859d5d98e913b1b891db9bb3c7ab0c065
MicheleCasani <casanimichele@gmail.com>
2025-04-15 14:46:55 +0200

    [UPD] add operations.js and exoprt on file index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const { addition, subtraction, multiplication, division } = require('./operations.js');
+
+console.log(addition(3,4))
+console.log(subtraction(3,4))
+console.log(multiplication(3,4))
+console.log(division(3,4))
+
+// --------------------------------------------------------------
+
 const message = process.argv[2]
 
 console.log(message)
+
+
+
